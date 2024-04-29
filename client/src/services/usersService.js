c594ce343f7c6351aaf4db3c09bf40548a33f7f6
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-04-29 22:26:09 +0700

    del st

--- client/src/services/usersService.js
+++ client/src/services/usersService.js
@@ -67,32 +67,4 @@ const resetPasswordUser = async (id, token, password) => {
   // }
 };
 
-//***********************************************LOGIN WITH GOOGLE ACCOUNT************************** */
-const loginGoogleUser = async () => {
-  // useEffect(() => {
-  //   const getUser = () => {
-  //     fetch("http://localhost:5000/auth/login/success", {
-  //       method: "GET",
-  //       credentials: "include",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //         "Access-Control-Allow-Credentials": true,
-  //       },
-  //     })
-  //       .then((response) => {
-  //         if (response.status === 200) return response.json();
-  //         throw new Error("authentication has been failed!");
-  //       })
-  //       .then((resObject) => {
-  //         setUser(resObject.user);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   };
-  //   getUser();
-  // }, []);
-};
-
 export { loginUser, loginGoogleUser, checkEmailUser, resetPasswordUser };
